735ecd633cac2f67d787294642a45363c58543ec
Greg Zaal <gregzzmail@gmail.com>
2021-04-27 13:06:42 +0200

    Use CORS

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const cors = require('cors')
 require('dotenv').config()
 
 const app = express();
+app.use(cors())
 
 const assetsRoute = require('./routes/assets');
 app.use('/assets', assetsRoute);
