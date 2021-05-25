90840a977281bdbe45d1e95631e9df99fbec0f9d
Greg Zaal <gregzzmail@gmail.com>
2021-05-25 14:29:20 +0200

    Store latest patron info in a table with uid as keys for easier lookup

--- routes/patreon_hook.js
+++ routes/patreon_hook.js
@@ -8,6 +8,19 @@ const bodyParser = require('body-parser')
 
 router.use(bodyParser.text({ type: '*/*' }));
 
+const centsToRank = (c) => {
+  if (c <= 300) {
+    return 1
+  } else if (c <= 500) {
+    return 2
+  } else if (c <= 1000) {
+    return 3
+  } else if (c <= 5000) {
+    return 4
+  }
+  return 5
+}
+
 router.post('/', async (req, res) => {
   const webhookSecret = process.env.PATREON_HOOK_SECRET;
   const hash = crypto.createHmac('md5', webhookSecret).update(req.body).digest('hex');
@@ -44,6 +57,33 @@ router.post('/', async (req, res) => {
 
   const docID = Date.now().toString() + Math.floor(Math.random() * 1000).toString().padStart(3, '0');
   db.collection('patreon_hooks').doc(docID).set(data)
+
+  let uid = "";
+  try {
+    uid = data.data.relationships.user.data.id;
+  } catch (e) {
+    res.status(400).json({
+      error: "400",
+      message: "Couldn't get uid"
+    })
+    return
+  }
+
+  let patron = {}
+  patron.uid = uid
+  patron.name = data.data.attributes.full_name
+  patron.joined = data.data.attributes.pledge_relationship_start
+  patron.rank = centsToRank(data.data.attributes.currently_entitled_amount_cents)
+  patron.last_charge_date = data.data.attributes.last_charge_date
+  patron.last_charge_status = data.data.attributes.last_charge_status
+  patron.next_charge_date = data.data.attributes.next_charge_date
+  patron.status = data.data.attributes.patron_status
+  const entitled_tiers = data.data.relationships.currently_entitled_tiers
+  if (entitled_tiers.length > 0) {
+    patron.tier = data.data.relationships.currently_entitled_tiers[0].id
+    // Deliberately not setting `else patron.tier = null` so we can always know what tier a past patron was on.
+  }
+  db.collection('patrons').doc(uid).set(patron)
 
   res.status(200).json({
     message: "OK"
