c0dceb366a3fdc657e82255ea82da51a3b9fdc68
Greg Zaal <gregzzmail@gmail.com>
2021-07-08 20:02:03 +0200

    Add PATCH endpoint to set patron info. Better key authorization

--- routes/patron_info.js
+++ routes/patron_info.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const escape = require('escape-html');
 const express = require('express');
 const router = express.Router();
@@ -11,15 +12,23 @@ require('dotenv').config()
 router.use(express.json())
 
 router.post('/', async (req, res) => {
-  if (req.body.key !== process.env.PATRON_INFO_KEY) {
+  if (!req.body.uuid || !req.body.key) {
+    res.status(400).json({
+      error: "400",
+      message: "Bad request."
+    })
+    return
+  }
+
+  const uuid = req.body.uuid
+  const hash = crypto.createHmac('md5', process.env.PATRON_INFO_KEY).update(uuid).digest('hex')
+  if (req.body.key !== hash) {
     res.status(403).json({
       error: "403 Forbidden",
       message: "Incorrect key"
     })
     return
   }
-
-  const uuid = req.body.uuid
 
   if (!uuid) {
     res.status(400).json({
@@ -38,6 +47,44 @@ router.post('/', async (req, res) => {
   } else {
     res.status(200).json(doc.data());
   }
+});
+
+router.patch('/', async (req, res) => {
+  if (!req.body.uuid || !req.body.key) {
+    res.status(400).json({
+      error: "400",
+      message: "Bad request."
+    })
+    return
+  }
+
+  const uuid = req.body.uuid
+  const hash = crypto.createHmac('md5', process.env.PATRON_INFO_KEY).update(uuid).digest('hex')
+  if (req.body.key !== hash) {
+    res.status(403).json({
+      error: "403 Forbidden",
+      message: "Incorrect key"
+    })
+    return
+  }
+
+  if (!uuid) {
+    res.status(400).json({
+      error: "400 Bad Request",
+      message: "uuid is undefined"
+    })
+    return
+  }
+
+  let data = JSON.parse(JSON.stringify(req.body))
+  delete data.uuid
+  delete data.key
+
+  const doc = await db.collection('patrons').doc(uuid);
+
+  doc.update(data)
+
+  res.status(200).json(data);
 });
 
 module.exports = router;
