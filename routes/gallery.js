5e7fae1e2d4d7431671fc3a6f3606b2608f9fb4c
Greg Zaal <gregzzmail@gmail.com>
2021-07-14 17:51:49 +0200

    Add render gallery endpoint

---
+++ routes/gallery.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const router = express.Router();
+
+const firestore = require('../firestore');
+
+const db = firestore();
+
+const popularity = (num, epoch) => {
+  return num / Math.pow(Math.abs(Date.now() - epoch) + 1, 1.7)
+}
+
+router.get('/', async (req, res) => {
+  const assetID = req.query.assetID;
+  const limit = req.query.limit;
+
+  let collectionRef = db.collection('gallery');
+
+  // Get all renders if request is for a specific asset, otherwise only favourited ones
+  if (assetID) {
+    collectionRef = collectionRef.where('asset_used', '==', assetID);
+  } else {
+    collectionRef = collectionRef.where('favourite', '==', true);
+  }
+
+  if (parseInt(limit)) {
+    collectionRef = collectionRef.limit(parseInt(limit))
+  }
+
+  const collection = await collectionRef.get();
+  let docs = {};
+  collection.forEach(doc => {
+    docs[doc.id] = doc.data();
+  });
+
+  // Filter unpublished
+  for (const id in docs) {
+    if (docs[id].approval_pending) {
+      delete docs[id];
+    }
+  }
+
+  // Sort by popularity
+  let sortedKeys = Object.keys(docs).sort(function (a, b) {
+    return (popularity(docs[b].clicks, docs[b].date_added) - popularity(docs[a].clicks, docs[a].date_added));
+  })
+  let data = []
+  for (const k of sortedKeys) {
+    data.push(docs[k])
+  }
+
+  res.status(200).json(data);
+});
+
+module.exports = router;
