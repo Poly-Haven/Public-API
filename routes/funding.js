5840696dd298009cfd795521462acb8ed7100f48
Greg Zaal <gregzzmail@gmail.com>
2021-07-27 12:40:41 +0200

    Keep new corporate tiers separate from individual donations.

--- routes/funding.js
+++ routes/funding.js
@@ -1,17 +1,33 @@
 const express = require('express');
 const router = express.Router();
 
+const patreon_tiers = require('../constants/patreon_tiers.json')
+
 const firestore = require('../firestore');
 
 router.get('/', async (req, res) => {
   const db = firestore();
 
+  let corporateTierIDs = [];
+  for (const [id, data] of Object.entries(patreon_tiers)) {
+    if (data.rewards.includes("Corporate Silver")) {
+      corporateTierIDs.push(id)
+    }
+  }
+
   const colPatrons = await db.collection('patrons').where('status', '==', 'active_patron').get();
   let numPatrons = 0
   let sumCents = 0
   colPatrons.forEach(doc => {
-    numPatrons += 1
-    sumCents += doc.data().cents || 0
+    const data = doc.data()
+    let ignore = false;
+    if (data.tiers && data.tiers.some(ai => corporateTierIDs.includes(ai))) {
+      ignore = true
+    }
+    if (!ignore) {
+      numPatrons += 1
+      sumCents += data.cents || 0
+    }
   });
 
   const colCorp = await db.collection('corporate_sponsors').where('rank', '>', 0).get();
