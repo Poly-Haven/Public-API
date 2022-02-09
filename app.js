6f12697e3ab49c03f6a6b747a8b8f158dffd276a
Greg Zaal <gregzzmail@gmail.com>
2022-02-09 18:14:55 +0200

    Add OpenAPI documentation

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const cors = require('cors')
 const fs = require('fs')
+const YAML = require('yamljs');
 require('dotenv').config()
 
 const app = express();
 app.use(cors())
+
+const swaggerDocument = YAML.load('./swagger.yml');
+app.get("/api-docs/swagger.json", (req, res) => res.json(swaggerDocument))
 
 const routeDir = "./routes/"
 fs.readdir(routeDir, (err, files) => {
@@ -24,7 +28,7 @@ fs.readdir(routeDir, (err, files) => {
 app.get('/', (req, res) => {
   res.status(200).send(`Welcome to the Poly Haven API!
   Documentation for available endpoints is here:
-  https://github.com/Poly-Haven/Public-API`);
+  https://api.polyhaven.com/api-docs/`);
 })
 
 app.listen(3000);
