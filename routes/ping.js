d178406ca983bf136d63db2d0cc75a60e2c0b0af
Greg Zaal <gregzzmail@gmail.com>
2022-05-13 20:51:31 +0200

    Include latest commit message in ping response

--- routes/ping.js
+++ routes/ping.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
+const revision = require('child_process')
+  .execSync('git rev-list --format=oneline --max-count=1 HEAD')
+  .toString().trim()
+
+
 router.get('/', async (req, res) => {
   res.status(200).json({
     time: Date.now(),
     node: process.env.NODE_ID || "UNKNOWN",
+    commit: revision,
   });
 });
 
