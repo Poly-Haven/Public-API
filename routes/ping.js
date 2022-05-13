19a2c2740c3d7c54538442c115e8b5f4e7794ec1
Greg Zaal <gregzzmail@gmail.com>
2022-05-13 10:10:25 +0200

    Include hashed IP in ping response to identify load balance node

--- routes/ping.js
+++ routes/ping.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const crypto = require('crypto');
 const os = require("os");
+
+const interface = os.networkInterfaces()["ens3"]
+let nodeID = "UNKNOWN"
+if (interface) {
+  const ip = interface[1].address
+  nodeID = crypto.createHmac('sha256', process.env.DL_KEY)
+    .update(ip)
+    .digest('hex')
+    .substring(0, 4)
+}
 
 router.get('/', async (req, res) => {
   res.status(200).json({
     message: "Pong!",
     time: Date.now(),
-    node: os.hostname(),
+    node: nodeID,
   });
 });
 
