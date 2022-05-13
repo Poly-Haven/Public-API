29fe3971a69d8be5344357fdb69c85a9749178fb
Greg Zaal <gregzzmail@gmail.com>
2022-05-13 09:52:46 +0200

    Include hostname in ping response

--- routes/ping.js
+++ routes/ping.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const os = require("os");
 
 router.get('/', async (req, res) => {
-  res.status(200).json({ message: "Pong!", time: Date.now() });
+  res.status(200).json({
+    message: "Pong!",
+    time: Date.now(),
+    node: os.hostname(),
+  });
 });
 
 module.exports = router;
