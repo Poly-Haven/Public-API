bfccaaa3ce3c4f23e2daf4f03025cc4b91f73e18
Greg Zaal <gregzzmail@gmail.com>
2022-05-13 17:34:43 +0200

    OR! We just store an env var node ID.

--- routes/ping.js
+++ routes/ping.js
@@ -1,22 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const crypto = require('crypto');
-const os = require("os");
-
-const interface = os.networkInterfaces()["ens3"]
-let nodeID = "UNKNOWN"
-if (interface) {
-  const ip = interface[1].address
-  nodeID = crypto.createHmac('sha256', process.env.DL_KEY)
-    .update(ip)
-    .digest('hex')
-    .substring(0, 4)
-}
 
 router.get('/', async (req, res) => {
   res.status(200).json({
     time: Date.now(),
-    node: nodeID,
+    node: process.env.NODE_ID || "UNKNOWN",
   });
 });
 
