c914e138c4abbeab57a46224df2d43f3e52bb9aa
Greg Zaal <gregzzmail@gmail.com>
2022-07-26 14:24:19 +0200

    Add translation_progress endpoint

---
+++ routes/translation_progress.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+const firestore = require('../firestore');
+
+const db = firestore();
+
+router.get('/', async (req, res) => {
+  const doc = await db.collection('locales').doc('translation_progress').get();
+  res.status(200).json(doc.data());
+});
+
+module.exports = router;
