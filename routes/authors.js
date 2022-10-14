67376e3c3411b9f3cf6fcea6a3d2a683a07065e4
Greg Zaal <gregzzmail@gmail.com>
2022-10-14 09:49:09 +0200

    Include author asset count in /authors endpoint

--- routes/authors.js
+++ routes/authors.js
@@ -6,15 +6,35 @@ const firestore = require('../firestore');
 const db = firestore();
 
 router.get('/', async (req, res) => {
-  let collectionRef = db.collection('authors');
 
-  const collection = await collectionRef.get();
-  let docs = {};
-  collection.forEach(doc => {
-    docs[doc.id] = doc.data();
+  const colAssets = await db.collection('assets').get();
+  const assets = {};
+  colAssets.forEach(doc => {
+    assets[doc.id] = doc.data();
   });
 
-  res.status(200).json(docs);
+  const assetCounts = {};
+  for (const asset of Object.values(assets)) {
+    for (const author of Object.keys(asset.authors)) {
+      if (assetCounts[author]) {
+        assetCounts[author]++;
+      } else {
+        assetCounts[author] = 1;
+      }
+    }
+  }
+
+  const colAuthors = await db.collection('authors').get();
+  const authors = {};
+  colAuthors.forEach(doc => {
+    const ac = assetCounts[doc.id] || 0;
+    if (ac !== 0) {
+      authors[doc.id] = doc.data();
+      authors[doc.id].assetCount = assetCounts[doc.id] || 0;
+    }
+  });
+
+  res.status(200).json(authors);
 });
 
 module.exports = router;
