ef74542528a12605bd451f366298d4dc19a01b0f
Greg Zaal <gregzzmail@gmail.com>
2022-10-14 13:19:48 +0200

    Add /patrons_top endpoint

---
+++ routes/patrons_top.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const router = express.Router();
+const isBadWord = require('../utils/isBadWord')
+
+const firestore = require('../firestore');
+
+router.get('/', async (req, res) => {
+  const db = firestore();
+  const collection = await db.collection('patrons').orderBy('lifetime_cents', 'desc').limit(30).get();
+
+  // These are/were corporate sponsors (not individuals)
+  const ignored = [
+    'Graswald',
+    'Andrew_D',
+    'Luma Animation',
+  ]
+
+  let patrons = [];
+  collection.forEach(doc => {
+    const data = doc.data()
+    if (!data.anon) {
+      const name = data.display_name || data.name
+      if (!isBadWord(name) && !ignored.includes(name)) {
+        patrons.push([data.uid, name, Date.parse(data.joined)])
+      }
+    }
+  });
+
+  res.set("Cache-Control", `max-age=${30 * 60}`)
+  res.status(200).json(patrons)
+});
+
+module.exports = router;
