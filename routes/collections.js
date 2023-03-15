211d441f2ab168967eae1a4f1f0fd636ceb41273
Greg Zaal <gregzzmail@gmail.com>
2023-03-15 11:21:16 +0200

    Include partial asset list in collection

--- routes/collections.js
+++ routes/collections.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
+const sortObjBySubObjProp = require('../utils/sortObjBySubObjProp')
 const firestore = require('../firestore')
 
 const db = firestore()
@@ -11,6 +12,18 @@ router.get('/', async (req, res) => {
   collection.forEach((doc) => {
     docs[doc.id] = doc.data()
   })
+
+  // Get list of assets in each collection
+  for (const id in docs) {
+    const colAssets = await db.collection('assets').where('tags', 'array-contains', `collection: ${id}`).get()
+    let assets = {}
+    colAssets.forEach((doc) => {
+      assets[doc.id] = doc.data()
+    })
+    const sortedKeys = Object.keys(sortObjBySubObjProp(assets, 'download_count'))
+    docs[id].assets = sortedKeys.slice(0, 10)
+  }
+
   res.status(200).json(docs)
 })
 
