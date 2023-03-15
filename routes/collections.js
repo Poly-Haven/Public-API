142a10ae0db7d8d4437aa7c41f41a04aaee281a1
Greg Zaal <gregzzmail@gmail.com>
2023-03-15 11:06:54 +0200

    Add collections endpoint

--- routes/collections.js
+++ routes/collections.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const router = express.Router()
 
+const firestore = require('../firestore')
+
+const db = firestore()
+
 router.get('/', async (req, res) => {
-  res.status(501).send('Coming soon')
+  const collection = await db.collection('collections').orderBy('date_published', 'desc').get()
+  let docs = {}
+  collection.forEach((doc) => {
+    docs[doc.id] = doc.data()
+  })
+  res.status(200).json(docs)
 })
 
 module.exports = router
