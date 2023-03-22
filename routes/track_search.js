a18967c23d0b895cdf73a2d9834a3229716372ad
Greg Zaal <gregzzmail@gmail.com>
2023-03-22 16:26:45 +0200

    Add search tracking endpoint

---
+++ routes/track_search.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const router = express.Router()
+
+require('dotenv').config()
+
+router.use(express.json())
+
+router.post('/', async (req, res) => {
+  if (req.body.key !== process.env.DL_KEY) {
+    res.status(403).json({
+      error: '403 Forbidden',
+      message: 'Incorrect key',
+    })
+    return
+  }
+
+  const data = req.body
+  delete data.key
+  data.timestamp = new Date().toISOString()
+
+  const admin = require('firebase-admin')
+
+  if (!admin.apps.length) {
+    console.log('Firebase init')
+    admin.initializeApp({
+      credential: admin.credential.cert({
+        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      }),
+    })
+  }
+
+  const db = admin.firestore()
+
+  await db.collection('searches').add(data)
+
+  res.status(200).json({
+    message: 'OK',
+  })
+})
+
+module.exports = router
