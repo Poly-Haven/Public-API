6b08a062b1f8cb0ffc8b3829f5ec731aab15cf0f
Greg Zaal <gregzzmail@gmail.com>
2025-10-22 17:03:36 +0200

    Add /v2/files

---
+++ routes/v2/files.js
@@ -0,0 +1,41 @@
+const escape = require('escape-html')
+const express = require('express')
+const router = express.Router()
+
+const firestore = require('../firestore')
+
+const db = firestore()
+
+router.get('/', (req, res) => {
+  res.status(400).send(`Please format your request as /files/[asset_id]`)
+})
+
+router.get('/:id', async (req, res) => {
+  const asset_id = req.params.id
+
+  if (!asset_id) {
+    res.status(400).send(`No asset with that ID`)
+    return
+  }
+
+  const MAX_ID_LENGTH = 50
+  if (asset_id.length > MAX_ID_LENGTH) {
+    res.status(400).send(`No asset with that ID`)
+    return
+  }
+
+  const validPattern = /^[a-zA-Z0-9_-]+$/
+  if (!validPattern.test(asset_id)) {
+    res.status(400).send(`No asset with that ID`)
+    return
+  }
+
+  const doc = await db.collection('files').doc(asset_id).get()
+  if (!doc.exists) {
+    res.status(404).send(`No asset with id ${escape(asset_id)}`)
+  } else {
+    res.status(200).json(doc.data())
+  }
+})
+
+module.exports = router
