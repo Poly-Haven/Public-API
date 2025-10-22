df00e8639fbf98609f6ddc370f0cd6cbb0232bc8
Greg Zaal <gregzzmail@gmail.com>
2025-10-22 17:21:36 +0200

    Add key requirement to /v2/files

--- routes/v2/files.js
+++ routes/v2/files.js
@@ -2,9 +2,8 @@ const escape = require('escape-html')
 const express = require('express')
 const router = express.Router()
 
-const firestore = require('../firestore')
-
-const db = firestore()
+const cachedFirestore = require('../../utils/cachedFirestore')
+const db = cachedFirestore()
 
 router.get('/', (req, res) => {
   res.status(400).send(`Please format your request as /files/[asset_id]`)
@@ -13,29 +12,94 @@ router.get('/', (req, res) => {
 router.get('/:id', async (req, res) => {
   const asset_id = req.params.id
 
-  if (!asset_id) {
-    res.status(400).send(`No asset with that ID`)
-    return
+  // Key must be provided
+  const authHeader = req.headers.authorization
+  if (!authHeader) {
+    return res.status(403).json({
+      error: '403 Forbidden',
+      message: 'Please provide an API key in the Authorization header',
+    })
   }
 
-  const MAX_ID_LENGTH = 50
-  if (asset_id.length > MAX_ID_LENGTH) {
-    res.status(400).send(`No asset with that ID`)
-    return
+  if (!asset_id) {
+    return res.status(400).json({
+      error: '400 Bad Request',
+      message: 'No asset ID provided',
+    })
   }
 
   const validPattern = /^[a-zA-Z0-9_-]+$/
   if (!validPattern.test(asset_id)) {
-    res.status(400).send(`No asset with that ID`)
-    return
+    return res.status(400).json({
+      error: '400 Bad Request',
+      message: 'Invalid asset ID',
+    })
   }
 
-  const doc = await db.collection('files').doc(asset_id).get()
-  if (!doc.exists) {
-    res.status(404).send(`No asset with id ${escape(asset_id)}`)
-  } else {
-    res.status(200).json(doc.data())
+  // Validate API key
+  const apiKey = authHeader.startsWith('Bearer ') ? authHeader.substring(7) : authHeader
+  const allowedKeyChars = 'abcdef0123456789'
+  if (apiKey.length !== 64 || [...apiKey].some((c) => !allowedKeyChars.includes(c))) {
+    return res.status(403).json({
+      error: '403 Forbidden',
+      message: 'Invalid API key format',
+    })
   }
+  let keyDoc = await db.collection('api_keys').doc(apiKey).get()
+  if (!keyDoc.exists) {
+    return res.status(403).json({
+      error: '403 Forbidden',
+      message: 'Invalid API key',
+    })
+  }
+  const keyData = keyDoc.data()
+  if (keyData.status !== 'active') {
+    return res.status(403).json({
+      error: '403 Forbidden',
+      message: 'API key is not active',
+    })
+  }
+
+  let includeUpcoming = false
+
+  // For Superhive customers, we always include early access
+  if (keyData.superhive_uid) {
+    includeUpcoming = true
+  }
+
+  // For patrons, we need to check if they have a sufficient tier and that it's active
+  // TODO
+  // if (keyData.patron_uid) {
+
+  const filesDoc = await db.collection('files').doc(asset_id).get()
+  if (!filesDoc.exists) {
+    return res.status(404).json({
+      error: '404 Not Found',
+      message: `No asset with id ${escape(asset_id)}`,
+    })
+  }
+
+  // Check if the asset is upcoming/early access
+  const infoDoc = await db.collection('assets').doc(asset_id).get()
+  const infoData = infoDoc.data() // We can assume it exists since the file list exists
+  if (infoData.staging) {
+    return res.status(403).json({
+      error: '403 Forbidden',
+      message: 'Asset is in staging',
+    })
+  }
+  const now = Math.floor(Date.now() / 1000)
+  if (!includeUpcoming && infoData.date_published > now) {
+    return res.status(403).json({
+      error: '403 Forbidden',
+      message: 'Asset is in early access',
+    })
+  }
+
+  return res.status(200).json({
+    message: 'OK',
+    data: filesDoc.data(),
+  })
 })
 
 module.exports = router
